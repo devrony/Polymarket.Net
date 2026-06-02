fe300e589ac5850556ca870d6c5690eed48794ec
Jkorf <jankorf91@gmail.com>
2026-06-02 09:16:26 +0200

    Fixed user client provider not caching new client when previous client was disposed

--- Polymarket.Net/Clients/PolymarketUserClientProvider.cs
+++ Polymarket.Net/Clients/PolymarketUserClientProvider.cs
@@ -84,7 +84,7 @@ namespace Polymarket.Net.Clients
             if (credentials != null)
             {
                 client.SetApiCredentials(credentials);
-                _restClients.TryAdd(userIdentifier, client);
+                _restClients[userIdentifier] = client;
             }
             return client;
         }
@@ -96,7 +96,7 @@ namespace Polymarket.Net.Clients
             if (credentials != null)
             {
                 client.SetApiCredentials(credentials);
-                _socketClients.TryAdd(userIdentifier, client);
+                _socketClients[userIdentifier] = client;
             }
             return client;
         }
